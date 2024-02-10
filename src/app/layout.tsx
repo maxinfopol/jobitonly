2a7133a3fb676bf655f40a0c530f94f6513250fa
Parvi <parvinders2k@gmail.com>
2024-02-10 18:08:11 +0530

    job site

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "#1 Job Board for tech industry in India",
+  description: "IT job offers, Tech Jobs, Startup Jobs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/Footer/footer";
+import Header from "@/components/Header/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
